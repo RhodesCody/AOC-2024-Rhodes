ed78c9fc43c4ec29106f4b97535bbb9bd02c4497
RhodesCody <cody.rhodes@expedient.com>
2024-12-02 18:10:16 -0500

    day1 - part1

---
+++ day1/day1.py
@@ -0,0 +1,21 @@
+first = []
+second = []
+ans = []
+import os
+cwd = os.getcwd()
+with open("day1\input_day1") as file:
+    while line := file.readline():
+        newline = line.rstrip().split(' ')
+        first.append(int(newline[0]))
+        second.append(int(newline[len(newline)-1]))
+    first.sort()
+    second.sort()
+    for i in range(0, len(first)):
+        x = abs(int(first[i]) - int(second[i]))
+        ans.append(x)
+
+z = 0
+for i in ans:
+    z += i
+print(ans)
+print(z)

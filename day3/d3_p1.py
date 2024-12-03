41fc59320b3c364df8535b4e976d142047f00cef
RhodesCody <109983909+RhodesCody@users.noreply.github.com>
2024-12-03 14:38:35 -0500

    day3 - p1

---
+++ day3/d3_p1.py
@@ -0,0 +1,25 @@
+import re
+
+def mult(list):
+    """
+    :param list: list of num pairs to multiply
+    :return int: result of adding up all mult pairs
+    """
+    ans = 0
+    for i in list:
+        x = int(i[0]) * int(i[1])
+        ans += x
+    return ans
+
+z = 0
+
+with open("day3\input_day3.txt") as file:
+    while line := file.readline().rstrip():
+        #level = line.split()
+        #match = re.search(r'mul\((-?\d+),(-?\d+)\)', line)
+        #print(match)
+        match2 = re.findall(r'mul\((-?\d+),(-?\d+)\)', line)
+        #print(match2)
+        z += mult(match2)
+
+print(z)

e64ce8f97c359eb209713501a1ea9c0f32d12668
RhodesCody <109983909+RhodesCody@users.noreply.github.com>
2024-12-03 13:32:20 -0500

    Update d2_p2.py
    p2 solved

--- day2/d2_p2.py
+++ day2/d2_p2.py
@@ -1,54 +1,49 @@
 def is_safe(level):
     """
-    Now with added problem solver capability!
-
     :param level: list of nums to check
     :return int: 0 = not safe | 1 = safe
     """
     tmp = []
-    cnt_pos = 0
-    cnt_neg = 0
-    grace = True
-    rt = 0
-    for i in range(0, len(level)-1):
+    for i in range(0,len(level)-1):
             x = int(level[i]) - int(level[i+1])
-            if (x > 0):
-                cnt_pos += 1
-            if (x < 0):
-                cnt_neg += 1
-            
-            y = -1
-            if (i+2) <= len(level)-1:
-                y = int(level[i]) - int(level[i+2])
-            
             if abs(x) > 3:
-                if grace and abs(y) <= 3:
-                    rt = 1
-                    grace = False
-                else: 
-                    return 0
+                return 0
             if abs(x) == 0:
-                if abs(y) != 0 and grace:
-                    rt = 1
-                    grace = False
-                else:
-                    return 0
+                return 0
             tmp.append(x)
+    if (all(int(num) < 0 for num in tmp)):
+        return 1
+    if (all(int(num) > 0 for num in tmp)):
+        return 1
+    return 0
 
-    if cnt_neg > 0 and cnt_pos <= 1 and grace:
-        rt = 1
-    elif cnt_pos > 0 and cnt_neg <= 1 and grace:
-        rt = 1
-    
-    return rt
+def problem_solver(level):
+    """
+    Try to solve bad levels by removing 1 item at a time and re-checking
+
+    :param level: list of nums to check
+    :return int: 0 = not safe | 1 = safe
+    """
+    chk = 0
+    for i in range(0, len(level)):
+        tmp = level.copy()
+        del(tmp[i])
+        chk = is_safe(tmp)
+        if chk == 1:
+            return chk
+    return chk
+
 
 safety = []
 with open("day2\input_day2") as file:
     while line := file.readline().rstrip():
         level = line.split()
-        safety.append(is_safe(level))
+        chk = is_safe(level)
+        if chk == 0:
+            chk = problem_solver(level)
+        safety.append(chk)
 z = 0
 for i in safety:
     z += i
-print(safety)
+#print(safety)
 print(z)
